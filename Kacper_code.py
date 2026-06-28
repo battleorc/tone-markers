6ae3d57688f91c33057904f7a82660b01a3cacf5
battleorc <kacper.szlapinski@gmail.com>
2026-06-28 14:29:35 +0200

    tables

--- Kacper_code.py
+++ Kacper_code.py
@@ -2,6 +2,7 @@ from scipy.stats import chi2_contingency
 import pandas as pd
 import numpy as np
 import statsmodels.api as sm
+import matplotlib.pyplot as plt
 df = pd.read_csv("Kacper_data/SARC/pol/data.csv")
 
 
@@ -54,3 +55,30 @@ print("Logistic Regression:")
 print(model.summary())
 print("Odds Ratio")
 print(odds_ratio)
+
+#Table1
+
+ax = tab_pct.plot(kind="bar", stacked=True)
+
+ax.set_xlabel("Sarcasm")
+ax.set_ylabel("Percentage")
+ax.set_title("Formality distribution by sarcasm")
+ax.legend(["Non-formal", "Formal"], title="Formality")
+plt.xticks([0,1], ["Non-sarcastic", "Sarcastic"], rotation=0)
+plt.tight_layout()
+plt.savefig("Kacper_data/formality_distribution_by_sarcasm.png", dpi = 300, bbox_inches="tight")
+plt.close()
+
+#Table2
+
+formal_rate = df.groupby("is_sarcasm")["is_formal"].mean() * 100
+
+ax = formal_rate.plot(kind="bar")
+
+ax.set_xlabel("Sarcasm")
+ax.set_ylabel("Formal comments (%)")
+ax.set_title("Percentage of formal comments by sarcasm")
+plt.xticks([0,1], ["Non-sarcastic", "Sarcastic"], rotation = 0)
+plt.tight_layout()
+plt.savefig("Kacper_data/percentage_formal_comments_by_sarcasm.png", dpi = 300, bbox_inches="tight")
+plt.close()
