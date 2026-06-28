ae3d3751ef635215a3b146c82e5faba0c4452128
battleorc <kacper.szlapinski@gmail.com>
2026-06-28 14:16:00 +0200

    statistical tests

--- Kacper_code.py
+++ Kacper_code.py
@@ -1,4 +1,7 @@
+from scipy.stats import chi2_contingency
 import pandas as pd
+import numpy as np
+import statsmodels.api as sm
 df = pd.read_csv("Kacper_data/SARC/pol/data.csv")
 
 
@@ -15,3 +18,39 @@ tab_pct = pd.crosstab(
 ) * 100
 
 print(tab_pct)
+
+#Chi Squared
+
+chi2, p, dof, expected = chi2_contingency(tab_cross)
+
+print("chi2:", chi2)
+print("p-value", p)
+print("degrees of freedom:", dof)
+print("expected:")
+print(expected)
+
+if p < 0.05:
+    print("There is a statistically significant relation between sarcasm and formality")
+else:
+    print("There is no basis for statistical relation")
+
+#Cramer's V
+n = tab_cross.to_numpy().sum()
+cramers_v = np.sqrt(chi2 / n)
+
+print("Cramer's V:", cramers_v)
+
+#Logistic Regression
+X = df[["is_sarcasm"]]
+X = sm.add_constant(X)
+y = df["is_formal"]
+
+model = sm.Logit(y, X).fit()
+
+#odds ratio
+odds_ratio = np.exp(model.params)
+
+print("Logistic Regression:")
+print(model.summary())
+print("Odds Ratio")
+print(odds_ratio)
